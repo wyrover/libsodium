724cda6e80cef4c7e462a713ab58551c5f452eba
Konstantinos Sofokleous <ksophocleous@gmail.com>
2015-01-16 09:49:55 +0100

    added missing resource files for vs builds

---
+++ msvc/resource.rc.h
@@ -0,0 +1,105 @@
+// Microsoft Visual C++ generated resource script.
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+#define APSTUDIO_READONLY_SYMBOLS
+/////////////////////////////////////////////////////////////////////////////
+//
+// Generated from the TEXTINCLUDE 2 resource.
+//
+#include "winres.h"
+
+/////////////////////////////////////////////////////////////////////////////
+#undef APSTUDIO_READONLY_SYMBOLS
+
+/////////////////////////////////////////////////////////////////////////////
+// English (United States) resources
+
+#if !defined(AFX_RESOURCE_DLL) || defined(AFX_TARG_ENU)
+LANGUAGE LANG_ENGLISH, SUBLANG_ENGLISH_US
+
+#ifdef APSTUDIO_INVOKED
+/////////////////////////////////////////////////////////////////////////////
+//
+// TEXTINCLUDE
+//
+
+1 TEXTINCLUDE 
+BEGIN
+    "resource.h\0"
+END
+
+2 TEXTINCLUDE 
+BEGIN
+    "#include ""winres.h""\r\n"
+    "\0"
+END
+
+3 TEXTINCLUDE 
+BEGIN
+    "\r\n"
+    "\0"
+END
+
+#endif    // APSTUDIO_INVOKED
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// Version
+//
+
+VS_VERSION_INFO VERSIONINFO
+ FILEVERSION 0,@SODIUM_LIBRARY_VERSION_MAJOR@,@SODIUM_LIBRARY_VERSION_MINOR@,@SODIUM_LIBRARY_VERSION_PATCH@
+ PRODUCTVERSION 0,@SODIUM_LIBRARY_VERSION_MAJOR@,@SODIUM_LIBRARY_VERSION_MINOR@,@SODIUM_LIBRARY_VERSION_PATCH@
+ FILEFLAGSMASK 0x3fL
+#ifdef _DEBUG
+ FILEFLAGS 0x1L
+#else
+ FILEFLAGS 0x0L
+#endif
+ FILEOS 0x40004L
+ FILETYPE 0x7L
+ FILESUBTYPE 0x0L
+BEGIN
+    BLOCK "StringFileInfo"
+    BEGIN
+        BLOCK "040904b0"
+        BEGIN
+            VALUE "CompanyName", "libsodium contributors"
+            VALUE "FileDescription", "libsodium"
+            VALUE "FileVersion", "0.@SODIUM_LIBRARY_VERSION_MAJOR@.@SODIUM_LIBRARY_VERSION_MINOR@.@SODIUM_LIBRARY_VERSION_PATCH@"
+            VALUE "InternalName", "libsodium"
+            VALUE "LegalCopyright", "(c) Frank Denis (attribution required)"
+            VALUE "OriginalFilename", "libsodium.dll"
+            VALUE "ProductName", "libsodium"
+            VALUE "ProductVersion", "0.@SODIUM_LIBRARY_VERSION_MAJOR@.@SODIUM_LIBRARY_VERSION_MINOR@.@SODIUM_LIBRARY_VERSION_PATCH@"
+        END
+    END
+    BLOCK "VarFileInfo"
+    BEGIN
+        VALUE "Translation", 0x409, 1200
+    END
+END
+
+#endif    // English (United States) resources
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+#ifndef APSTUDIO_INVOKED
+/////////////////////////////////////////////////////////////////////////////
+//
+// Generated from the TEXTINCLUDE 3 resource.
+//
+
+
+/////////////////////////////////////////////////////////////////////////////
+#endif    // not APSTUDIO_INVOKED
